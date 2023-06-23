756b5f55055bd3cae96b9f7de3c690c8a4555ccd
Frederikusvm <117975812+Frederikusvm@users.noreply.github.com>
2023-06-23 10:41:24 +0200

    wp confick

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress' );
+define( 'DB_NAME', 's170049_Themebuilder' );
 
 /** Database username */
-define( 'DB_USER', 'bit_academy' );
+define( 'DB_USER', 's170049_Themebuilder' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'bit_academy' );
+define( 'DB_PASSWORD', 'Themebuilder123' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
